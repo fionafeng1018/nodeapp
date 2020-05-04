33c70749b4199b49f7cf02c70a16a79bbcb853b6
Fiona Feng <fiona.feng@pearson.com>
2020-05-03 21:03:33 -0400

    update

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser =  require('body-parser');
+require('dotenv/config');
 
-//Middlewares
-app.use('/posts', ()=>{
-    console.log('This is a middleware running');
-});
+app.use(bodyParser.json());
+
+//Middlewares example
+// app.use('/posts', () => {
+//     console.log('This is a middleware running');
+// });
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
 
 //ROUTES
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("home page");
 });
-
-app.get('/posts', (req, res) => {
-    res.send("posts page");
-})
-
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION,  
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("connected to DB!")
+);
 
 //Start listening to the server
 app.listen(3000);
-
